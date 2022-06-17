7eed4d57d89b2b32c3414489e0690d9cbbec38a1
yamat0jp <yamat0jp@yahoo.co.jp>
2022-06-18 06:43:05 +0900

    これでできるんじゃないかなあと思っています

---
+++ linuxbbs.dpr
@@ -0,0 +1,16 @@
+program linuxbbs;
+
+{$APPTYPE CONSOLE}
+
+uses
+  Web.WebBroker,
+  CGIApp,
+  WebModuleUnit1 in 'WebModuleUnit1.pas' {WebModule1: TWebModule};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.WebModuleClass := WebModuleClass;
+  Application.Run;
+end.
